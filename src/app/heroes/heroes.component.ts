d08792d4235bb136abd78ccbd5a72b7ef87fbba7
arun_focaloid <arunkr@focaloid.com>
2019-05-02 10:52:55 +0530

    dkwd

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from './hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
   ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './heroes/hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'arun' },
+    { id: 12, name: 'sarath' },
+    { id: 13, name: 'umesh' },
+    { id: 14, name: 'jibin' },
+    { id: 15, name: 'unni' },
+    { id: 16, name: 'sajan' },
+    { id: 17, name: 'vipin' },
+    { id: 18, name: 'adarsh' },
+    { id: 19, name: 'krishnajith' },
+    { id: 20, name: 'suneera' }
+]
